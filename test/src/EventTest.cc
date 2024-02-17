cb60dca96dfd29443f53abf868b442dfc2ce7ef7
razerx100 <saikatmandal8@gmail.com>
2024-02-17 18:35:18 +0000

    Add tests for Unsubscribing

--- test/src/EventTest.cc
+++ test/src/EventTest.cc
@@ -39,20 +39,23 @@ class User
 public:
 	class Dispatchable : public ITitanDispatchable<TestEventType>
 	{
+		std::string_view m_name;
 	public:
+		Dispatchable(std::string_view name) : m_name{ std::move(name) } {}
+
 		void ProcessEvent(const TestEventBaseType& eventt) override
 		{
 			if (eventt.GetType() == TestEventType::A)
-				std::cout << static_cast<size_t>(eventt.GetType()) << "\n";
+				std::cout << m_name << " " << static_cast<size_t>(eventt.GetType()) << "\n";
 			else if (eventt.GetType() == TestEventType::B)
 			{
 				const EventB& eventB = eventt.CastType<EventB>();
-				std::cout << eventB.GetString() << "\n";
+				std::cout << m_name << " " << eventB.GetString() << "\n";
 			}
 		}
 	};
 
-	User() : m_eventReceiver{ std::make_shared<Dispatchable>() } {}
+	User(std::string_view name) : m_eventReceiver{ std::make_shared<Dispatchable>( name ) } {}
 
 	void SubscribeToA(TitanTestDispatcher& dispatcher)
 	{
@@ -62,25 +65,33 @@ public:
 	{
 		dispatcher.Subscribe(TestEventType::B, m_eventReceiver);
 	}
+	void UnsubscribeToA(TitanTestDispatcher& dispatcher)
+	{
+		dispatcher.Unsubscribe(TestEventType::A, m_eventReceiver);
+	}
+	void UnsubscribeToB(TitanTestDispatcher& dispatcher)
+	{
+		dispatcher.Unsubscribe(TestEventType::B, m_eventReceiver);
+	}
 
 private:
 	std::shared_ptr<Dispatchable> m_eventReceiver;
 };
 
-TEST(TitanEventTest, EventTest)
+TEST(TitanEventTest, EventSubscribeTest)
 {
 	TitanTestDispatcher testDispatcher{};
 
-	User user{};
+	User user{ "User" };
 	user.SubscribeToA(testDispatcher);
 
 	{
-		User user1{};
+		User user1{ "TempUser1" };
 		user1.SubscribeToA(testDispatcher);
 		user1.SubscribeToB(testDispatcher);
 	}
 
-	User user1{};
+	User user1{ "User1" };
 	user1.SubscribeToA(testDispatcher);
 	user1.SubscribeToB(testDispatcher);
 
@@ -89,4 +100,22 @@ TEST(TitanEventTest, EventTest)
 
 	EventB eventB{};
 	testDispatcher.Dispatch(eventB);
+}
+
+TEST(TitanEventTest, EventUnsubscribeTest)
+{
+	TitanTestDispatcher testDispatcher{};
+
+	User user{ "User" };
+	user.SubscribeToA(testDispatcher);
+
+	User user1{ "User1" };
+	user1.SubscribeToA(testDispatcher);
+
+	EventA eventA{};
+	testDispatcher.Dispatch(eventA);
+
+	user1.UnsubscribeToA(testDispatcher);
+
+	testDispatcher.Dispatch(eventA);
 }
